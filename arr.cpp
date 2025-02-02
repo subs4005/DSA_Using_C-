f956b1cb9bb2d345360501fc5431215140cc74e1
@4005sub <4005subs@gmail>
2025-02-02 17:31:18 +0530

    DSA_Recursion

---
+++ arr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void f(int *arr, int idx, int n){
+
+    if(idx == n) return;
+    cout<<arr[idx]<<"\n";
+    f(arr, idx+1, n);
+}
+
+int main(){
+    int n = 5;
+    int arr[] = {1,5,8,3,5};
+    f(arr, 0, n);
+    return 0;
+}
